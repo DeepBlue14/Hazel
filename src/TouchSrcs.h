05fecd5ed1e762774b988ab96b264eeb93195c81
DeepBlue14 <james.perl12@gmail.com>
2015-08-03 19:26:12 -0400

    added reconfig files

---
+++ src/TouchSrcs.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   TouchSrcs.h
+ * Author: james
+ * Email: 
+ * File Description: 
+ *
+ * Created on August 3, 2015, 7:24 PM
+ */
+
+#ifndef TOUCH_SRCS_H
+#define	TOUCH_SRCS_H
+
+
+
+#endif	/* TOUCH_SRCS_H */
