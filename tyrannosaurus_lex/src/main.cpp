b005e9255c2e2a75bb06be2724d2185030a394b7
DeepBlue14 <james.perl12@gmail.com>
2015-12-02 01:03:37 -0500

    updated T-lex module

--- tyrannosaurus_lex/src/main.cpp
+++ tyrannosaurus_lex/src/main.cpp
@@ -14,6 +14,8 @@
 #include "clang/Tooling/Tooling.h"
 #include "llvm/Support/raw_ostream.h"
 
+#include "IfStmtHandler.h"
+
 using namespace clang;
 using namespace clang::ast_matchers;
 using namespace clang::driver;
@@ -21,129 +23,34 @@ using namespace clang::tooling;
 
 static llvm::cl::OptionCategory ToolingSampleCategory("Matcher Sample");
 
-class IfStmtHandler : public MatchFinder::MatchCallback {
-public:
-  IfStmtHandler(Replacements *Replace) : Replace(Replace) {}
-
-  virtual void run(const MatchFinder::MatchResult &Result) {
-    // The matched 'if' statement was bound to 'ifStmt'.
-    if (const IfStmt *IfS = Result.Nodes.getNodeAs<clang::IfStmt>("ifStmt")) {
-      const Stmt *Then = IfS->getThen();
-      Replacement Rep(*(Result.SourceManager), Then->getLocStart(), 0,
-                      "// the 'if' part\n");
-      Replace->insert(Rep);
-
-      if (const Stmt *Else = IfS->getElse()) {
-        Replacement Rep(*(Result.SourceManager), Else->getLocStart(), 0,
-                        "// the 'else' part\n");
-        Replace->insert(Rep);
-      }
-    }
-  }
-
-private:
-  Replacements *Replace;
-};
-
-int main(int argc, const char **argv) {
-  CommonOptionsParser op(argc, argv, ToolingSampleCategory);
-  RefactoringTool Tool(op.getCompilations(), op.getSourcePathList());
-
-  // Set up AST matcher callbacks.
-  IfStmtHandler HandlerForIf(&Tool.getReplacements());
-
-  MatchFinder Finder;
-  Finder.addMatcher(ifStmt().bind("ifStmt"), &HandlerForIf);
-
-  // Run the tool and collect a list of replacements. We could call runAndSave,
-  // which would destructively overwrite the files with their new contents.
-  // However, for demonstration purposes it's interesting to show the
-  // replacements.
-  if (int Result = Tool.run(newFrontendActionFactory(&Finder).get())) {
-    return Result;
-  }
-
-  llvm::outs() << "Replacements collected by the tool:\n";
-  for (auto &r : Tool.getReplacements()) {
-    llvm::outs() << r.toString() << "\n";
-  }
-
-  return 0;
-}
 
 
-/*
-#include "clang/AST/ASTConsumer.h"
-#include "clang/AST/RecursiveASTVisitor.h"
-#include "clang/Frontend/CompilerInstance.h"
-#include "clang/Frontend/FrontendAction.h"
-#include "clang/Tooling/Tooling.h"
 
-using namespace clang;
-
-
-class FindNamedClassVisitor : public RecursiveASTVisitor<FindNamedClassVisitor>
+int main(int argc, const char **argv)
 {
-    public:
-        explicit FindNamedClassVisitor(ASTContext* Context) : Context(Context)
-        {
-            ;
-        }
+    CommonOptionsParser op(argc, argv, ToolingSampleCategory);
+    RefactoringTool Tool(op.getCompilations(), op.getSourcePathList() );
 
-        bool VisitCXXRecordDecl(CXXRecordDecl *Declaration)
-        {
-            if(Declaration->getQualifiedNameAsString() == "n::m::C")
-            {
-                FullSourceLoc fullLocation = Context->getFullLoc(Declaration->getLocStart() );
-                if(fullLocation.isValid() )
-                {
-                    llvm::outs() << "Found declaration at " << fullLocation.getSpellingLineNumber() << ":" << fullLocation.getSpellingColumnNumber() << "\n";
-                }    
-            }
-            return true;
-        }
+    // Set up AST matcher callbacks.
+    IfStmtHandler HandlerForIf(&Tool.getReplacements() );
 
-private:
-    ASTContext *Context;
-    
-}; // End of class FindNamedClassVisitor
+    MatchFinder Finder;
+    Finder.addMatcher(ifStmt().bind("ifStmt"), &HandlerForIf);
 
-
-class FindNamedClassConsumer : public clang::ASTConsumer
-{
-    public:
-        explicit FindNamedClassConsumer(ASTContext *Context) : Visitor(Context)
-        {
-            ;
-        }
-
-        virtual void HandleTranslationUnit(clang::ASTContext &Context)
-        {
-            Visitor.TraverseDecl(Context.getTranslationUnitDecl());
-        }
-
-    private:
-        FindNamedClassVisitor Visitor;
-
-}; // End of class FindNamedClassConsumer
-
-
-class FindNamedClassAction : public clang::ASTFrontendAction
-{
-    public:
-        virtual std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(clang::CompilerInstance &Compiler, llvm::StringRef InFile)
-        {
-            return std::unique_ptr<clang::ASTConsumer>(new FindNamedClassConsumer(&Compiler.getASTContext()) );
-        }
-}; // End of class FindNamedClassAction
-
-
-//--------------------------------------
-int main(int argc, char **argv)
-{
-    if(argc > 1)
+    // Run the tool and collect a list of replacements. We could call runAndSave,
+    // which would destructively overwrite the files with their new contents.
+    // However, for demonstration purposes it's interesting to show the
+    // replacements.
+    if(int Result = Tool.run(newFrontendActionFactory(&Finder).get()) )
     {
-        clang::tooling::runToolOnCode(new FindNamedClassAction, argv[1]);
+        return Result;
     }
+
+    llvm::outs() << "Replacements collected by the tool:\n";
+    for (auto &r : Tool.getReplacements() )
+    {
+        llvm::outs() << r.toString() << "\n";
+    }
+
+    return 0;
 }
-*/
--- tyrannosaurus_lex/src/test.cpp
+++ tyrannosaurus_lex/src/test.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    //cout << "Hello World!" << endl;
+    cout << "Hello World!" << endl;
     int num = 0;
 
     if(num == 0)
