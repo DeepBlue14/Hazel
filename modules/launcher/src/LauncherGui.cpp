c067f1193c0ef784571e44ee2a7dd4287799ac55
DeepBlue14 <james.perl12@gmail.com>
2016-09-06 08:09:35 -0400

    updated launcher code

--- modules/launcher/src/LauncherGui.cpp
+++ modules/launcher/src/LauncherGui.cpp
@@ -1,3 +1,5 @@
+//drag&drop ref: http://doc.qt.io/qt-4.8/qabstractitemview.html
+
 #include "LauncherGui.h"
 
 
@@ -5,23 +7,31 @@ LauncherGui::LauncherGui(QWidget* parent) : QWidget(parent)
 {
     numOfNodes = 0;
     tableWidgetPtr = new QTableWidget();
+    
+    tableWidgetPtr->setDragEnabled(true);
+    tableWidgetPtr->setDragDropMode(QAbstractItemView::InternalMove);
+    tableWidgetPtr->setSelectionMode(QAbstractItemView::SingleSelection);
+    tableWidgetPtr->setSelectionBehavior(QAbstractItemView::SelectRows);
+    
     runablesTblWidItmPtrVecPtr = new QVector<QTableWidgetItem*>();
 
     cout << "adding table widgets" << endl;
     tableWidgetPtr->setRowCount(5);
     tableWidgetPtr->setColumnCount(2);
     tableWidgetPtr->setHorizontalHeaderItem(0, new QTableWidgetItem("Execution Command") );
-    tableWidgetPtr->setHorizontalHeaderItem(1, new QTableWidgetItem("Order") );
+    tableWidgetPtr->setHorizontalHeaderItem(1, new QTableWidgetItem("Delay") );
 
     QTableWidgetItem* item = new QTableWidgetItem("rosrun test_node test_node");
     item->setFlags(item->flags() | Qt::ItemIsUserCheckable);
     item->setCheckState(Qt::Checked);
     
+    tableWidgetPtr->setItem(0, 0, item);
+    
+    /*
     QSpinBox* spinBoxPtr = new QSpinBox();
     spinBoxPtr->setMaximum(numOfNodes);
-    tableWidgetPtr->setItem(0, 0, item);
     tableWidgetPtr->setCellWidget(0, 1, spinBoxPtr);
-
+    */
     
     westTabWidgetPtr = new QTabWidget();
     
